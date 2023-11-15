3686230138949f34067867f1a49f87d47f211210
ommer.ahmad <46134431+ohahmad@users.noreply.github.com>
2023-11-15 21:51:39 +0000

    lint + prettier combo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,20 @@
 /* eslint-env node */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/strict-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier'
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   root: true,
   rules: {
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    'prettier/prettier': 2 // Means error
   },
   parserOptions: {
     project: true,
-    tsconfigRootDir: __dirname,
-  },
+    tsconfigRootDir: __dirname
+  }
 };
